78f2ffe94fdd69bc2a47f32d8e0027f19b9d882d
Seiga AL Ghifari <seigaalghi@gmail.com>
2022-01-20 09:14:04 +0700

    ! change password reset table

--- migrations/20220119025955-create-password-reset.js
+++ migrations/20220119025955-create-password-reset.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('passwordResets', {
+    await queryInterface.createTable('PasswordResets', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('passwordResets');
+    await queryInterface.dropTable('PasswordResets');
   }
 };
