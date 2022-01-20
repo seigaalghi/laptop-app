8853f29258c109f790fbf8f0b50b9ca440cc051e
Seiga AL Ghifari <seigaalghi@gmail.com>
2022-01-20 09:54:07 +0700

    + cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const route = require("./routes");
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req, res)=>{
